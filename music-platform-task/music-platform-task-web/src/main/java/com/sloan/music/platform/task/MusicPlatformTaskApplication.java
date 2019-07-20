3e9eb93e9f3c26ab9e0c7d05b08c64a721c0d0e0
zhaozhong <zhaozhong@youzan.com>
2019-07-20 18:42:28 +0800

    【task模块】

---
+++ music-platform-task/music-platform-task-web/src/main/java/com/sloan/music/platform/task/MusicPlatformTaskApplication.java
@@ -0,0 +1,17 @@
+package com.sloan.music.platform.task;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kakaluote zhaozhong@youzan.com
+ * @date 2019/7/20
+ **/
+public class MusicPlatformTaskApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MusicPlatformTaskApplication.class,args);
+    }
+}
